7e1414f28708138dc3084b88f9e06f9f1748044f
ds4978019-source <ds4978019@gmail.com>
2026-01-16 14:11:52 +0530

    Create MainActivity.java

---
+++ app/src/main/app/src/main/java/com/streamverse/MainActivity.java
@@ -0,0 +1,13 @@
+package com.streamverse;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Ekhane Firebase aar AdMob-er logic thakbe
+    }
+}
